1765be62a026e345b5a43f72258f8f2d6e5a2411
sunway910 <shengweizhong910@gmail.com>
2024-06-25 17:21:08 +0800

    call webhook with plaintext

--- watchdog/internal/util/webhook.go
+++ watchdog/internal/util/webhook.go
@@ -1,12 +1,11 @@
 package util
 
 import (
-	"bytes"
-	"encoding/json"
 	"github.com/CESSProject/watchdog/constant"
 	"github.com/CESSProject/watchdog/internal/model"
 	"log"
 	"net/http"
+	"strings"
 )
 
 type WebhookConfig struct {
@@ -14,9 +13,12 @@ type WebhookConfig struct {
 }
 
 func (conf *WebhookConfig) SendAlertToWebhook(content model.AlertContent) (err error) {
-	jsonContent, err := json.Marshal(content)
+	sendData := `{
+		"msg_type": "text",
+		"content": {"text": "` + "CESS Information: " + " Alert Time: " + content.AlertTime + ", Host: " + content.HostIp + ", Miner Name: " + content.ContainerName + ", Description: " + content.Description + `"}
+	}`
 	for i := 0; i < len(conf.Webhooks); i++ {
-		req, err := http.NewRequest("POST", conf.Webhooks[i], bytes.NewBuffer(jsonContent))
+		req, err := http.NewRequest("POST", conf.Webhooks[i], strings.NewReader(string(sendData)))
 		req.Header.Set("Content-Type", "application/json")
 		if err != nil {
 			log.Fatal("Failed to create new http request client")
@@ -33,7 +35,6 @@ func (conf *WebhookConfig) SendAlertToWebhook(content model.AlertContent) (err e
 }
 
 func sendRequest(req *http.Request) error {
-	req.Header.Set("Content-Type", "application/json")
 	client := &http.Client{}
 	for j := 0; j < constant.HttpMaxRetry; j++ {
 		resp, err := client.Do(req)
