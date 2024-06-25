a67abe39ef011edb92704b6176c8da0c2e0e1203
sunway910 <shengweizhong910@gmail.com>
2024-06-25 17:18:50 +0800

    fix TimeFormat err

--- watchdog/constant/constant.go
+++ watchdog/constant/constant.go
@@ -7,7 +7,7 @@ const (
 	GenBlockInterval = 6 // unit: second
 	ConfPath         = "/opt/monitor/config.yaml"
 	HttpMaxRetry     = 3
-	TimeFormat       = "2006-06-06 06:06:06"
+	TimeFormat       = "2006-01-02 15:04:05"
 )
 
 const (
