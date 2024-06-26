9dd274c28dc216d0d20df8cfbfba19ccac90454d
sunway910 <shengweizhong910@gmail.com>
2024-06-26 18:21:11 +0800

    clean constant properties

--- watchdog/constant/constant.go
+++ watchdog/constant/constant.go
@@ -1,7 +1,5 @@
 package constant
 
-import "time"
-
 const (
 	MinerImage       = "cesslab/cess-miner"
 	GenBlockInterval = 6 // unit: second
@@ -11,13 +9,7 @@ const (
 )
 
 const (
-	LogMaxAge    = 86400 * time.Second
-	RotationTime = 604800 * time.Second
-)
-
-const (
 	Size1kib = 1024
 	Size1mib = 1024 * Size1kib
 	Size1gib = 1024 * Size1mib
-	Size1tib = 1024 * Size1gib
 )
