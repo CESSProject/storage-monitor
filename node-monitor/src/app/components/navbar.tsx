5f64945463da28d713aa7f331351b8c3d90d48e9
sunway910 <shengweizhong910@gmail.com>
2024-07-01 23:04:11 +0800

    bugfix: change to camel case

--- node-monitor/src/app/components/navbar.tsx
+++ node-monitor/src/app/components/navbar.tsx
@@ -29,8 +29,8 @@ export default function NavBar() {
                                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
                                 <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
                                      xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 20">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                          stroke-width="2"
+                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
+                                          strokeWidth="2"
                                           d="M1 17V2a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H3a2 2 0 0 0-2 2Zm0 0a2 2 0 0 0 2 2h12M5 15V1m8 18v-4"/>
                                 </svg>
                             </a>
@@ -45,14 +45,14 @@ export default function NavBar() {
                                             <ellipse cx="14.5" cy="9" rx="1.5" ry="1"/>
                                         </g>
                                     </mask>
-                                    <g fill="none" stroke="currentColor" stroke-linecap="round"
-                                       stroke-linejoin="round" stroke-width="2">
-                                        <path stroke-dasharray="30" stroke-dashoffset="30"
+                                    <g fill="none" stroke="currentColor" strokeLinecap="round"
+                                       strokeLinejoin="round" strokeWidth="2">
+                                        <path strokeDasharray="30" strokeDashoffset="30"
                                               d="M12 4C13.6683 4 14.6122 4.39991 15 4.5C15.5255 4.07463 16.9375 3 18.5 3C18.8438 4 18.7863 5.21921 18.5 6C19.25 7 19.5 8 19.5 9.5C19.5 11.6875 19.017 13.0822 18 14C16.983 14.9178 15.8887 15.3749 14.5 15.5C15.1506 16.038 15 17.3743 15 18C15 18.7256 15 21 15 21M12 4C10.3317 4 9.38784 4.39991 9 4.5C8.47455 4.07463 7.0625 3 5.5 3C5.15625 4 5.21371 5.21921 5.5 6C4.75 7 4.5 8 4.5 9.5C4.5 11.6875 4.98301 13.0822 6 14C7.01699 14.9178 8.1113 15.3749 9.5 15.5C8.84944 16.038 9 17.3743 9 18C9 18.7256 9 21 9 21">
                                             <animate fill="freeze" attributeName="stroke-dashoffset" dur="0.6s"
                                                      values="30;0"/>
                                         </path>
-                                        <path stroke-dasharray="10" stroke-dashoffset="10" d="M9 19">
+                                        <path strokeDasharray="10" strokeDashoffset="10" d="M9 19">
                                             <animate fill="freeze" attributeName="stroke-dashoffset" begin="0.7s"
                                                      dur="0.2s" values="10;0"/>
                                             <animate
