8864c367d69b724b09fd7f5152e6c45773fef029
sunway910 <shengweizhong910@gmail.com>
2024-07-04 14:09:22 +0800

    make footer to bottom flex

--- node-monitor/src/app/layout.tsx
+++ node-monitor/src/app/layout.tsx
@@ -24,12 +24,12 @@ export default function RootLayout({children,}: Readonly<{ children: React.React
         <body className={inter.className}>
         <div className="w-screen h-screen dark:bg-black">
             <LeftDrawer/>
-            <div className="w-full flex-grow p-1 md:overflow-y-auto md:p-1">
+            <div className="w-full flex-grow p-1 md:overflow-y-auto md:p-1 mb-auto">
                 <NavBar/>
                 {children}
             </div>
-            <Footer/>
         </div>
+        <Footer/>
         </body>
         </html>
     );
