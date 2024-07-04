8f93d485bd18eee240d87068d6e4e46319a990db
sunway910 <shengweizhong910@gmail.com>
2024-07-04 14:07:53 +0800

    revert router

--- node-monitor/src/app/page.tsx
+++ node-monitor/src/app/page.tsx
@@ -4,6 +4,5 @@ import {useRouter} from "next/navigation";
 export default function Home() {
     const router = useRouter();
     router.push("/dashboard");
-    router.push("/system");
-    return <div>{}</div>;
+    return null;
 }
