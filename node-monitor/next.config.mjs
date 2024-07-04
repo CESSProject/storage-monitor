496fbc499d42b14a82fd949b2b6cf25d853872d9
sunway910 <shengweizhong910@gmail.com>
2024-07-04 14:06:56 +0800

    update build config

--- node-monitor/next.config.mjs
+++ node-monitor/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const nextConfig = {
+    output: 'export',
+    distDir: 'build',
+}
+
+module.exports = nextConfig
+
