de1038f2f1a6488ab15e18aea0ebb26a5645bc6e
sunway910 <shengweizhong910@gmail.com>
2024-07-09 17:48:09 +0800

    change env name

--- web/src/utils.ts
+++ web/src/utils.ts
@@ -1,7 +1,7 @@
 export const getApiServerUrl = () => {
     // TODO: get the url from .env.local file
     // const api_server_url = "http://localhost:3001";
-    const api_server_url = process.env.NEXT_PUBLIC_API_SERVER_URL;
+    const api_server_url = process.env.API_URL;
     return api_server_url;
 }
 
