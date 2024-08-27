22aab188e4009332235e86bc9ec11427f3ef0034
sunway910 <shengweizhong910@gmail.com>
2024-08-27 18:06:55 +0800

    feat: do not new chainClient repeatedly #6

---
+++ watchdog/internal/util/chainclient.go
@@ -0,0 +1,21 @@
+package util
+
+import (
+	"context"
+	cess "github.com/CESSProject/cess-go-sdk"
+	"github.com/CESSProject/cess-go-sdk/chain"
+	"time"
+)
+
+type CessChainClient struct {
+	CessClient *chain.ChainClient
+}
+
+func NewCessChainClient(rpcUrl []string) *CessChainClient {
+	chainClient, _ := cess.New(
+		context.Background(),
+		cess.ConnectRpcAddrs(rpcUrl),
+		cess.TransactionTimeout(time.Second*30),
+	)
+	return &CessChainClient{CessClient: chainClient}
+}
