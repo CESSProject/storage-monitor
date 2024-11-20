54a52a48d7efab7e7e1a2a2a906946029a6338d4
sunway910 <shengweizhong910@gmail.com>
2024-11-20 17:17:20 +0800

    chore: watchdog server clean code

--- watchdog/build.sh
+++
@@ -1,3 +0,0 @@
-# sh build.sh v0.0.1
-docker build -t cesslab/watchdog:"$1" .
-docker tag cesslab/watchdog:"$1" cesslab/watchdog:latest
--- watchdog/push.sh
+++
@@ -1,3 +0,0 @@
-# sh push.sh v0.0.1
-docker push cesslab/watchdog:"$1"
-docker push cesslab/watchdog:latest
